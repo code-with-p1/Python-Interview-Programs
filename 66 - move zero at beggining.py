3565f6238bfafccd62b67ef13d96cc0fae5de02c
Pawan Mane <pawanmane7644@gmail.com>
2025-08-07 23:30:47 +0530

    Latest Push

--- 66 - move zero at beggining.py
+++ 66 - move zero at beggining.py
@@ -9,3 +9,24 @@ arr = [0, 1, 0, 2, 0, 3, 4, 0, 5]
 print("Original List:", arr)
 move_zeros_to_beginning(arr)
 print("Modified List:", arr)
+
+print("#"*100)
+
+# Approach 2
+def move_zeros_to_beginning(arr):
+    n = len(arr)
+    pos = n - 1  # Pointer to place non-zero elements
+
+    # Traverse from end to start
+    for i in range(n-1, -1, -1):
+        if arr[i] != 0:
+            arr[pos] = arr[i]
+            if pos != i:
+                arr[i] = 0  # Replace original non-zero with 0
+            pos -= 1
+
+# Example
+arr = [0, 1, 0, 2, 0, 3, 4, 0, 5]
+print("Original List:", arr)
+move_zeros_to_beginning(arr)
+print("Modified List:", arr)
--- 67 - move zero to end.py
+++ 67 - move zero to end.py
@@ -1,10 +1,27 @@
+# Approach 1
 def move_zeros_to_end(arr):
     for i in range(len(arr)):
         if arr[i] == 0:
             arr.pop(i)
             arr.append(0)
 
-# Example
+arr = [10, 0, 20, 0, 30, 40, 0, 50, 0]
+print("Original List:", arr)
+move_zeros_to_end(arr)
+print("Modified List:", arr)
+
+print("#"*100)
+
+# Approach 2
+def move_zeros_to_end(arr):
+    pos = 0  # Position to place next non-zero
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[pos] = arr[i]
+            if i != pos:
+                arr[i] = 0  # Replace old spot with 0
+            pos += 1
+
 arr = [10, 0, 20, 0, 30, 40, 0, 50, 0]
 print("Original List:", arr)
 move_zeros_to_end(arr)
