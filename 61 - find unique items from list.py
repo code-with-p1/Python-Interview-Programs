c5bebfee64e2956aa8553ae8a0c52fa414963f3f
Pawan Mane <pawanmane7644@gmail.com>
2025-08-07 22:09:18 +0530

    New Changes

--- 61 - find unique items from list.py
+++ 61 - find unique items from list.py
@@ -1,3 +1,5 @@
+# Example 1
+
 list_data = [1,4,1,2,3,4,5,6,7,6,4]
 
 item_count = {}
@@ -11,3 +13,17 @@ for i in list_data:
 item_count = [key for (key, value) in item_count.items() if value == 1]
 
 print(item_count)
+
+
+# Example 2 
+from collections import Counter
+
+list_data = [1, 4, 1, 2, 3, 4, 5, 6, 7, 6, 4]
+
+# Count items
+item_count = Counter(list_data)
+
+# Extract items with count == 1
+unique_items = [item for item, count in item_count.items() if count == 1]
+
+print(unique_items)
